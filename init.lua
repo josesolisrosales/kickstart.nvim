5e54077de667898a72ea7388f3eab72bb4e046e7
Jose Solis <jose.solisrosales@sysdig.com>
2025-06-10 22:50:49 -0600

    Change suggestion keymap to avoid interfering with regular CR

--- init.lua
+++ init.lua
@@ -586,7 +586,7 @@ cmp.setup {
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
